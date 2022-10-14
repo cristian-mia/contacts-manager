f984288c984e2a91ff6fe537c34d81cd34c32549
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-10-14 12:12:43 -0500

    Contacts Manager

--- ContactsApplication.java
+++ ContactsApplication.java
@@ -1,7 +1,14 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ContactsApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Contacts newContact = new Contacts("name", "number");
         newContact.getName();
         newContact.setName("Cris");
+
+        ContactsManager.createPaths();
     }
 }
