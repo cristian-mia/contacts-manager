f4cecbc71091d7398a9a001192a6fbfb33a488d4
Cristian Almendarez <cristian.t.almendarez@gmail.com>
2022-10-14 11:50:11 -0500

    updates to Contacts

---
+++ Contacts.java
@@ -0,0 +1,20 @@
+public class Contacts {
+    private String name;
+    private String number;
+    public Contacts(String name, String number){
+        this.name = name;
+        this.number = number;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getNumber(){
+        return this.number;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setNumber(String number){
+        this.number = number;
+    }
+}
